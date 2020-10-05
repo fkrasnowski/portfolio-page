56ca475f78156e8383c5701c4c5839655e06796e
fkrasnowski <66143473+fkrasnowski@users.noreply.github.com>
2020-10-05 17:34:35 +0200

    Add plugin offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,8 @@ module.exports = {
         name: "Krasnowski",
         short_name: "Krasnowski",
         start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
+        background_color: "#1B1F23",
+        theme_color: "#1B1F23",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
@@ -45,5 +45,6 @@ module.exports = {
         crossOrigin: `use-credentials`,
       },
     },
+    `gatsby-plugin-offline`,
   ],
 }
